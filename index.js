d34c39caf7ebf9cceb0bf8232f62fbf3a0e8b04a
Mr-Randell <randell.onyango@student.moringaschool.com>
2022-08-04 16:49:25 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,44 @@
+function add() {
+    return 1+1;
+}
+function subtract() {
+    return 17-1;
+}
+function multiply() {
+    return 11*11;
+}
+function divide() {
+    return 15/3;
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+    return ++n;
+}
+
+function decrement(n) {
+    return --n;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+
+
+
+parseFloat("80.123999");
